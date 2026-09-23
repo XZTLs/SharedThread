920c288e1756f00c93dd6d8c60b30cd9452f5174
student <student@itismeucci.com>
2026-09-23 13:42:44 +0200

    changed random sleep from 200-500 to 100-500

--- src/main/java/it/xztl/example/Lavoratore.java
+++ src/main/java/it/xztl/example/Lavoratore.java
@@ -13,7 +13,7 @@ public class Lavoratore implements Runnable {
     public void run() {
         while (counter.increase(name)) {
             try {
-                Thread.sleep((long)(Math.random() * 300 + 200));
+                Thread.sleep((long)(Math.random() * 400 + 100));
             } catch (InterruptedException e) {
                 System.out.println(e);
             }
